1ed953415c2ce1c3b3c51c6bb34928bd97adf27a
alen devassy <alendevassy09@gmail.com>
2022-09-15 19:00:16 +0530

    atlas added

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports={
   con:()=>{
-    return mongoose.connect('mongodb://localhost:27017/project').then(()=>{
+    return mongoose.connect(process.env.DB).then(()=>{
       console.log('mongoose connected');
     }).catch((err)=>{
       console.log('not connected'+err);
